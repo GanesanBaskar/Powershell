3d68fbf2b3e9593c3ebca53e9ebd2c85d0d5fb13
GanesanBaskar <101430627+GanesanBaskar@users.noreply.github.com>
2022-03-12 13:16:10 +0530

    Update Powershell.ps1

--- Powershell.ps1
+++ Powershell.ps1
@@ -1,2 +1,3 @@
+$host = [System.Net.Dns]::GetHostName()
 $ver= (Get-WmiObject -Class win32_operatingsystem).Version
-write-host "The version" is $ver
+write-host "The version" is $ver "& the hostname is" $host
